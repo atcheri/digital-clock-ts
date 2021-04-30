6b81acc2f0c1c0eb00fe1d0c98211bef62128368
Atch <atcheri@laposte.net>
2021-04-30 18:56:16 +0200

    display the zone under the clock

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 const hours = document.querySelector('.hours');
 const minutes = document.querySelector('.minutes');
 const seconds = document.querySelector('.seconds');
+const zone = document.querySelector('.zone');
+const zoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
+zone!.innerHTML = zoneName.split('/')[1] || "";
 
 const second = 1000;
 
